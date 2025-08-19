b4cb19a4e8ca22c9fd99c0ef71d01ca3350e7545
rafdavis <rd506269@gmail.com>
2025-08-18 22:29:25 -0300

    feat: Update Aula 01

--- ldw-aula-01-introducao-ao-flask/app.py
+++ ldw-aula-01-introducao-ao-flask/app.py
@@ -15,7 +15,13 @@ def home(): # Função que será executada ao acessar a rota
 
 @app.route('/games')
 def games():
-    return render_template('games.html')
+    title = 'Tarisland'
+    year = 2022
+    category = 'MMORPG'
+    players = ['Yan', 'Ferrari', 'Valéria', 'Amanda']
+    # Dicionário em Python (objeto)
+    console = {'Nome' : 'Playstation 5', 'Fabricante' : 'Sony', 'Ano' : 2020 }
+    return render_template('games.html', title=title, year=year, category=category, players=players, console=console)
 
 # Se for o arquivo for executado diretamente pelo interpretador o servidor é iniciado
 if __name__ == '__main__':
